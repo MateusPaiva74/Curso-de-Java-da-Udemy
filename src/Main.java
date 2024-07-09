cb89274f2773bd56b44c3951f224881a1e096e6b
Mateus Andre <mateusandre74@gmail.com>
2024-07-09 16:43:39 -0300

    Polimorfismo

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
+        Pessoa pessoaFisica = new PessoaFisica();
+        Pessoa pessoaJuridica = new PessoaJuridica();
 
+        recebePessoa(pessoaJuridica);
+        recebePessoa(pessoaFisica);
+    }
+
+    public static void recebePessoa(Pessoa pessoa){
+        System.out.println("recebeu a pessoa");
     }
 }
